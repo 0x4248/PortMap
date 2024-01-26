b5fafc79ccc223a21e89b0476419d8189b8b4c1b
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-01-26 11:23:51 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PortMap
+
+A simple webpage that lists the ports you specify and their corresponding services for quick access.
+
+## Usage
+
+1. Clone the repository
+2. Edit the `ports.json` file to your liking
+3. Run `npm install flask`
+4. Run `python3 src/main.py`
+5. Open your ip address in your browser
---
+++ src/main.py
@@ -0,0 +1,23 @@
+# PortMap
+# A simple webpage that lists the ports you specify and their corresponding services for quick access.
+# Github: https://www.github.com/lewisevans2007/PortMap
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ports.json')
+def ports():
+    f = open('ports.json', 'r')
+    return f.read()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
